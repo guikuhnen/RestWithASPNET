b0461804b4f89d10b3657544e287709ba84317f0
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2024-06-24 20:51:07 -0300

    Content Negotiation.

--- RestWithASPNET/Program.cs
+++ RestWithASPNET/Program.cs
@@ -1,5 +1,6 @@
 using EvolveDb;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
 using MySqlConnector;
 using RestWithASPNET.Business;
 using RestWithASPNET.Model.Context;
@@ -20,6 +21,13 @@ namespace RestWithASPNET
 
 			var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
 			builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 3, 0))));
+
+			builder.Services.AddMvc(options =>
+			{
+				options.RespectBrowserAcceptHeader = true;
+				options.FormatterMappings.SetMediaTypeMappingForFormat("xml", MediaTypeHeaderValue.Parse("application/xml"));
+				options.FormatterMappings.SetMediaTypeMappingForFormat("json", MediaTypeHeaderValue.Parse("application/json"));
+			}).AddXmlSerializerFormatters();
 
 			builder.Services.AddApiVersioning();
 
@@ -51,7 +59,6 @@ namespace RestWithASPNET
 			app.UseHttpsRedirection();
 
 			app.UseAuthorization();
-
 
 			app.MapControllers();
 
